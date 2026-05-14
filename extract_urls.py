137a97207d40c9a53f1fc2b1fdb905c7cb8e7022
parth-trivedi-12 <parthtrivedi1207@gmail.com>
2026-05-14 15:58:53 +0530

    chore: remove unnecessary scraper scripts, raw html dumps, and output videos from repository tracking

--- extract_urls.py
+++
@@ -1,5 +0,0 @@
-import re
-html=open('homepage_logged_in.html', encoding='utf-8').read()
-matches = set(re.findall(r'//qx-shooz.myshopify.com/[^\s\'"<>]*\.(?:png|jpg|jpeg)', html))
-for m in matches:
-    print(m)
